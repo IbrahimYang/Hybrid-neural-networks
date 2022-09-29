2b9dd9ff731934b35258c1d375de9fb8b9175ba3
Zheyu.Yang <1711125768@qq.com>
2022-09-29 11:02:02 +0800

    Add files via upload
    Initialized updates of HSN

---
+++ HSN-hybrid sensing networks/aedat4_dataset.py
@@ -0,0 +1,95 @@
+import os
+import numpy as np
+import cv2
+from dv import AedatFile
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+
+
+class TurningDiskDataset(Dataset):
+    def __init__(self) -> None:
+        super().__init__()
+        self.events = np.load(abspath("aedat4_data/dvSave-2020_07_23_10_28_47_event.npy"))
+        self.frames = np.load(abspath("aedat4_data/dvSave-2020_07_23_10_28_47_frame.npy"))
+
+    def __len__(self):
+        return self.frames.shape[0]
+    
+    def __getitem__(self, item):
+        aps = self.frames[item]
+        dvs = self.events[item]
+        aps_loc = self.get_target(aps.transpose(1, 2, 0))
+        dvs_loc = np.stack([self.get_target(dvs[1:, ..., t].transpose(1, 2, 0), isFrame=False) for t in range(dvs.shape[-1])], -1)
+        return aps, dvs, aps_loc, dvs_loc
+
+    @staticmethod
+    def get_target(img, isFrame=True):
+        if isFrame:
+            img = cv2.threshold(img, 20, 255, cv2.THRESH_BINARY)[1].astype(np.uint8)
+        else:
+            img = (img > 0).astype(np.uint8)
+
+        contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        area_sort = np.argsort([cv2.contourArea(c) for c in contours])
+        area_sort = area_sort[-2:-5:-1] if isFrame else area_sort[:-4:-1]
+        contours = np.asarray(contours, dtype=object)[area_sort]
+
+        x, y, w, h = np.array([cv2.boundingRect(cnt) for cnt in contours]).T
+        xc, yc = x + w / 2, y + h / 2
+        center = np.stack([xc, yc], -1)
+        r = ((img.shape[1] / 2 - xc) ** 2 + (img.shape[0] / 2 - yc) ** 2) ** 0.5
+        center = center[np.argsort(r)]
+
+        center = center - np.array(img.shape[:2])[np.newaxis, ::-1] / 2 + 0.5
+        center = center / 8
+        return center
+
+
+def aedat4_2_numpy(file_path, T_event_frame=1, event_frame_num=5*2):
+    file_name_list = []
+    for file in os.listdir(file_path):
+        if "aedat4" in file:
+            file_name_list.append(file)
+    file_name_list.sort()
+
+    for file_name in file_name_list:
+        print("processing "+file_name+" ...")
+        file = AedatFile(abspath("aedat4_data/"+file_name))
+        event = next(file['events'])
+        start_frame_time = next(file['frames']).timestamp
+        blank_img = np.zeros((2,)+file['events'].size+(event_frame_num,), dtype=np.uint8)
+        frames_data, events_data = [], []
+
+        for frame in file['frames']:
+            print("frame time: {:.3f}ms".format((frame.timestamp - start_frame_time)/1e3))
+            img = cv2.resize(frame.image, (145, 145))[np.newaxis, ...]
+            frames_data.append(img)
+            frame_time = frame.timestamp - T_event_frame * event_frame_num / 2 * 1e3
+            c = 0
+            event_img = blank_img.copy()
+            while event.timestamp < frame_time:
+                event = next(file['events'])
+            while event.timestamp < frame.timestamp + T_event_frame * event_frame_num / 2 * 1e3:
+                while event.timestamp - frame_time < T_event_frame * 1e3:
+                    if event.polarity:
+                        event_img[0, event.y, event.x, c] += 1
+                    else:
+                        event_img[1, event.y, event.x, c] += 1
+                    event = next(file['events'])
+                frame_time += T_event_frame * 1e3
+                c += 1
+            cropped_img = event_img[:, 71:71+145, 88:88+145, :]
+            events_data.append(cropped_img)
+
+        np.save(abspath("aedat4_data/"+file_name[:file_name.find('.')]+"_frame.npy"), np.array(frames_data, dtype=np.float32))
+        np.save(abspath("aedat4_data/"+file_name[:file_name.find('.')]+"_event.npy"), np.array(events_data, dtype=np.float32))
+        file.close()
+
+
+def abspath(path):
+    return os.path.join(os.path.dirname(__file__), path)
+
+
+if __name__ == "__main__":
+    # aedat4_2_numpy(abspath("aedat4_data"))
+    data = TurningDiskDataset()[1]
---
+++ HSN-hybrid sensing networks/main.py
@@ -0,0 +1,84 @@
+from aedat4_dataset import TurningDiskDataset
+from model import TurningDiskSiamFC
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import cv2
+import os
+from pathlib import Path
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import MultiStepLR
+from tqdm import tqdm
+import time
+
+
+def train_siamfc():
+    epoch_num = 800
+    save_period = 2
+    load_ckpt_path = ""
+    save_ckpt_path = "ckpt/TurningDiskSiamFC_snn.ckpt"
+    Path(os.path.dirname(save_ckpt_path)).mkdir(parents=True, exist_ok=True)
+
+    writer = SummaryWriter('summary/train_predictor_{}'.format(int(time.time())))
+    net = TurningDiskSiamFC().cuda()
+    train_data = DataLoader(TurningDiskDataset(), batch_size=32, pin_memory=True, shuffle=True, num_workers=8)
+
+    if load_ckpt_path:
+        net.load_state_dict(torch.load(load_ckpt_path, map_location=torch.device("cuda:0")))
+
+    # net = torch.nn.DataParallel(net, device_ids=[0, 1, 2, 3])
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
+    scheduler = MultiStepLR(optimizer, milestones=[400], gamma=0.1)
+
+    for epoch in tqdm(range(epoch_num)):
+        for step, data in enumerate(train_data):
+            net_out = net(*[x.cuda() for x in data])
+            optimizer.zero_grad()
+            loss = net_out['loss'].mean()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(net.parameters(), 1)
+            optimizer.step()
+
+            writer.add_scalar('training loss', loss.item(), step + 1 + epoch * len(train_data))
+
+        scheduler.step()
+        if (epoch + 1) % save_period == 0:
+            print("\rsaving at epoch {}, loss={:.3f}, path: {}".format(epoch + 1, loss.item(), save_ckpt_path))
+            torch.save(net.state_dict(), save_ckpt_path)
+
+
+def test_siamfc():
+    load_ckpt_path = "ckpt/TurningDiskSiamFC_snn.ckpt"
+    net = TurningDiskSiamFC().cuda()
+    net.load_state_dict(torch.load(load_ckpt_path, map_location=torch.device("cuda:0")))
+    train_data = DataLoader(TurningDiskDataset(), batch_size=1, pin_memory=True, shuffle=False)
+
+    for step, data in enumerate(train_data):
+        aps, dvs, aps_loc, dvs_loc = data
+        net_out = net(aps.cuda(), dvs.cuda(), aps_loc.cuda(), dvs_loc.cuda(), training=False)
+
+        pred_loc = net_out['pred_loc'].squeeze().data.cpu().numpy().astype(np.int64)
+        gt_loc = net_out['gt_loc'].squeeze().data.cpu().numpy().astype(np.int64)
+        aps = aps.squeeze().data.cpu().numpy().astype(np.uint8)
+        dvs = dvs.squeeze()[1].data.cpu().numpy().astype(np.uint8)
+
+        for t in range(gt_loc.shape[2]):
+            plt.gca().clear()
+            img = cv2.cvtColor(aps, cv2.COLOR_GRAY2RGB)
+            img[dvs[..., t] != 0, 0] = 255
+            for o in range(gt_loc.shape[0]):
+                px, py = pred_loc[o, :, t]
+                gx, gy = gt_loc[o, :, t]
+                img = cv2.rectangle(img, (px-10, py-10), (px+10, py+10), (0, 0, 255), 1)
+                img = cv2.rectangle(img, (gx-10, gy-10), (gx+10, gy+10), (0, 255, 0), 1)
+                # label = "cross" if o == 0 else "triangle" if o == 1 else "circle"
+                # cv2.putText(img, label, (gx + 2, gy - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0, 255, 0), 1)
+
+            plt.imshow(img)
+            plt.pause(0.1)
+
+
+if __name__ == "__main__":
+    # train_siamfc()
+    test_siamfc()
